eca8e7abf42d7b57f38a9188ea1660ae76f75170
Satish Goda <satishgoda@live.com>
2025-04-25 20:26:24 -0700

    Create 4_Output.py
    Signed-off-by: Satish Goda <satishgoda@live.com>

---
+++ doc/decisions/DR007_code/4_Output.py
@@ -0,0 +1,6 @@
+Model Specification: {'vertices': 1000, 'edges': 2000, 'faces': 500}
+Shading Specification: {'shader_type': 'PBR', 'parameters': {'roughness': 0.5, 'metallic': 0.2}}
+Rigging Specification: {'bones': ['spine', 'arm', 'leg'], 'constraints': {'arm': 'IK', 'leg': 'IK'}}
+Animation Specification: {'keyframes': [{'frame': 1, 'pose': 'A'}, {'frame': 24, 'pose': 'B'}], 'duration': 1.0}
+Texturing Specification: {'texture_maps': {'diffuse': 'textures/diffuse.png', 'normal': 'textures/normal.png'}}
+Final Assembly Specification: {'components': ['Model', 'Shader', 'Rig', 'Animation', 'Texture']}
